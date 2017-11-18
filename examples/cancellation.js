d992ffb9c0cc20a02a9494a1348aceb46d52368b
Braden Napier <brady.napier@dashos.net>
2017-11-17 22:19:52 -0800

    add information about getNativeAsyncCost

--- examples/cancellation.js
+++ examples/cancellation.js
@@ -1,4 +1,13 @@
 /* @flow */
+
+/*
+  getNativeAsyncCost() is used on any example which utilizes node.js's
+  timers.  This is done to "fire up" the node.js timers which have a
+  delay on their first execution.
+
+  This gives us a more realistic look at the performance when utilizing
+  the performance.now() testing that our log() command provides.
+*/
 import { log, getNativeAsyncCost } from '../utils/log';
 
 import createTaskHandler from '../src';
